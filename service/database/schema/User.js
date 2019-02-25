290b6ea3f68a92b0bad44cdafdb39f74af9cd823
dj49846917 <821084785@qq.com>
2019-02-25 23:50:23 +0800

    优化了后端部分，可以使用了

--- service/database/schema/User.js
+++ service/database/schema/User.js
@@ -54,7 +54,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new Schema({
     UserId: {type:ObjectId},
